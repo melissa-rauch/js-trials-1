5030216a28abd66e380963e8754491117641364f
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-05-19 19:25:23 +0000

    Functions 1-5 translated to python

--- trials.py
+++ trials.py
@@ -2,24 +2,56 @@
 
 
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
+    """Print each item in list of items"""
+    for item in items:
+        print(item)
 
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
+    """Given a list of numbers, return a list of all even numbers"""
+    even_nums = []
+
+    for num in nums:
+        if (num % 2) == 0:
+            even_nums.append(num)
+
+    return even_nums
 
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
+    """Given a list of elements, return all elements at odd incices"""
+
+    result = []
+
+    for i in items:
+        if (i % 2) != 0:
+            result.append(items(i))
+
+    return result        
 
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+    """Given a list, output a numbered list"""
+
+    i = 1
+
+    while i < len(items):
+        for item in items:
+            print(f'{i}. {item}')
+            i = i + 1
 
 
 def get_range(start, stop):
-    pass  # TODO: replace this line with your code
+    """Return an array of numbers in a certain range"""
 
+    nums = []
+
+    for num in range(start, stop):
+        nums.append(num)
+
+    return nums
+    
+get_range(24, 35)
 
 def censor_vowels(word):
     pass  # TODO: replace this line with your code
